48a95f6d25dac013e9111e7be3d8bf378e20583d
Vladimir Vivien <vivienv@vmware.com>
2019-10-10 17:27:16 -0400

    Update release code

--- .ci/deploy/travis_deploy.go
+++ .ci/deploy/travis_deploy.go
@@ -11,21 +11,18 @@ func main() {
 	e := echo.New()
 	fmt.Println("Running binary release...")
 
-	// ensure we're in travis
-	if e.Eval("${TRAVIS}") == "" {
-		fmt.Println("This script can only run in Travis CI environment")
+	//ensure we're travis and configuree
+	if e.Eval("${GITHUB_TOKEN}") == "" {
+		fmt.Println("missing GITHUB_TOKEN env")
 		os.Exit(1)
 	}
 
 	// release on tag push
-	if !e.Empty("${TRAVIS_TAG}") {
-		fmt.Println("Releasing binaries with Goreleaser")
-		result := e.Run("curl -sL https://git.io/goreleaser | bash")
-		if result != "" {
-			fmt.Println("Goreleaser may have failed:", result)
-			os.Exit(1)
-		}
+	if e.Eval("${PUBLISH}") == "" {
+		fmt.Println("PUBLISH not set, skipping binary publish")
+		e.Runout("goreleaser --rm-dist --skip-validate --skip-publish")
 	} else {
-		fmt.Println("Not a tag pushed, skipping release")
+		fmt.Println("Publishing binaries with goreleaser")
+		e.Runout("goreleaser --rm-dist")
 	}
 }
