ae1319cfce814fe90943caa743159a04194af1f5
Benjamin Wang <wachao@vmware.com>
2022-09-30 14:58:13 +0800

    refactor the cmd and exec package
    move some duplicated code into common functions
    
    Signed-off-by: Benjamin Wang <wachao@vmware.com>

--- exec/executor.go
+++ exec/executor.go
@@ -8,30 +8,18 @@ import (
 	"io"
 	"os"
 
-	"github.com/pkg/errors"
 	"github.com/vmware-tanzu/crash-diagnostics/starlark"
 )
 
 type ArgMap map[string]string
 
 func Execute(name string, source io.Reader, args ArgMap) error {
-	star := starlark.New()
-
-	if args != nil {
-		starStruct, err := starlark.NewGoValue(args).ToStarlarkStruct("args")
-		if err != nil {
-			return err
-		}
-
-		star.AddPredeclared("args", starStruct)
-	}
-
-	err := star.Exec(name, source)
+	star, err := newExecutor(args)
 	if err != nil {
-		err = errors.Wrap(err, "exec failed")
+		return err
 	}
 
-	return err
+	return execute(star, name, source)
 }
 
 func ExecuteFile(file *os.File, args ArgMap) error {
@@ -44,15 +32,9 @@ type StarlarkModule struct {
 }
 
 func ExecuteWithModules(name string, source io.Reader, args ArgMap, modules ...StarlarkModule) error {
-	star := starlark.New()
-
-	if args != nil {
-		starStruct, err := starlark.NewGoValue(args).ToStarlarkStruct("args")
-		if err != nil {
-			return err
-		}
-
-		star.AddPredeclared("args", starStruct)
+	star, err := newExecutor(args)
+	if err != nil {
+		return err
 	}
 
 	// load modules
@@ -62,8 +44,26 @@ func ExecuteWithModules(name string, source io.Reader, args ArgMap, modules ...S
 		}
 	}
 
-	err := star.Exec(name, source)
-	if err != nil {
+	return execute(star, name, source)
+}
+
+func newExecutor(args ArgMap) (*starlark.Executor, error) {
+	star := starlark.New()
+
+	if args != nil {
+		starStruct, err := starlark.NewGoValue(args).ToStarlarkStruct("args")
+		if err != nil {
+			return nil, err
+		}
+
+		star.AddPredeclared("args", starStruct)
+	}
+
+	return star, nil
+}
+
+func execute(star *starlark.Executor, name string, source io.Reader) error {
+	if err := star.Exec(name, source); err != nil {
 		return fmt.Errorf("exec failed: %w", err)
 	}
 
